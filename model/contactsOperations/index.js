507dc4d7d8823a046d7124b02ffaa8a71c1b8a5a
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2021-12-05 12:51:46 +0200

    add contactsOperations folder

---
+++ model/contactsOperations/index.js
@@ -0,0 +1,13 @@
+const listContacts = require("./listContacts");
+const getContactById = require("./getContactById");
+const addContact = require("./addContact");
+const removeContactById = require("./removeContactById");
+const updateContacts = require("./updateContacts");
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContactById,
+  updateContacts,
+};
