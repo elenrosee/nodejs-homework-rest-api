6eb3bef2bcfbaa02877afbf4fb9c0aefbbf29a6c
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2022-01-09 15:15:18 +0200

    done

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,4 +1,5 @@
 const { hashSync, genSaltSync } = require('bcrypt')
+const gravatar = require('gravatar')
 const { Conflict } = require('http-errors')
 const { User } = require('../../models')
 
@@ -10,18 +11,25 @@ const signup = async (req, res, next) => {
     throw new Conflict('Email in use')
   }
 
+  const avatarURL = gravatar.url(email)
+
   const hashPassword = hashSync(password, genSaltSync(10))
   const result = await User.create({
     password: hashPassword,
     email,
     subscription,
+    avatarURL,
   })
 
   res.status(201).json({
     status: 'success',
     code: 201,
     data: {
-      user: { email, subscription: result.subscription },
+      user: {
+        email,
+        subscription: result.subscription,
+        avatarURL,
+      },
     },
   })
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,16 @@
 const express = require('express')
 
-const { controllerWrapper, auth } = require('../../middlewares')
+const { controllerWrapper, auth, upload } = require('../../middlewares')
 const { users: controllers } = require('../../controllers')
 
 const router = express.Router()
 
 router.get('/current', auth, controllerWrapper(controllers.getCurrent))
+router.patch(
+  '/avatars',
+  auth,
+  upload.single('avatar'),
+  controllerWrapper(controllers.updateAvatar)
+)
 
 module.exports = router
